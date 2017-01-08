b52e9b7778e7a285233a449cd160b8ce9da7ca7f
rPiBot <rPiBot>
2017-01-08 23:30:02 +0000

    v2

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import os, sys, time
 from datetime import datetime
+from thread
 
 from modules.camera import Camera
 from modules.body import Body
@@ -15,11 +16,7 @@ while True:
     time.sleep(0.01)
     Config.read_config(config)      # Keep reading config for changes
 
-    sensors = Sensors.retrieve(sensors)
+    sensors = thread.start_new_thread(Sensors.retrieve(sensors), ())
     print sensors   #TODO check they're updating
     Body.move(body, direction, sensors, config)
     Camera.move(camera, Config.retrieve(config, 'Camera', 'x'), Config.retrieve(config, 'Camera', 'y'))
-
-    # only if different from current:
-    #Camera.pan_tilt(camera, 'x', Config.retrieve('Camera', 'x'), 'step')
-    #Camera.pan_tilt(camera, 'y', Config.retrieve('Camera', 'y'), 'step')
