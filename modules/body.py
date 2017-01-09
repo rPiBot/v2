ddc8794cd849e357bc5212058cc7c7e6a22fb95d
rPiBot <rPiBot>
2017-01-09 13:55:44 +0000

    v2

--- modules/body.py
+++ modules/body.py
@@ -14,6 +14,9 @@ class Body:
     def __exit__(self):
         GPIO.cleanup()
 
+    def __init__(self):
+        self.stop()
+
     def stop(self, config):
         GPIO.output(35, False)
         GPIO.output(36, False)
@@ -23,28 +26,31 @@ class Body:
         Config.update_config(config, 'Body', 'direction', 'stopped')
 
     def move(self, direction, sensors, config):
-        if ((direction == 'forwards' or self.state == 'forwards') and sensors['F'] < 25) or ((direction == 'backwards' or self.state == 'backwards') and sensors['R'] < 25):
+        if (sensors['F'] < 25 or sensors['R'] < 25):
             print "Not safe to drive", direction
-            direction = 'stopped'
+            self.state = 'stopped'
 
         if self.state == 'evading':
             direction = 'stopped'
             self.state = 'stopped'
 
         if self.state == 'stopped': # Evade if necessary
-            if sensors['F'] < 25:
+            if sensors['F'] < 10:
                 direction = 'backwards'
                 self.state = 'evading'
 
-            if sensors['R'] < 25:
+            if sensors['R'] < 10:
                 direction = 'forwards'
                 self.state = 'evading'
 
-            if sensors['F'] < 25 and sensors['R'] < 25:
+            if sensors['F'] < 10 and sensors['R'] < 10:
                 direction = random.choice(['left', 'right'])
                 self.state = 'evading'
 
-        if direction != self.state or self.state == 'stopped':
+        if self.state == 'stopped' or direction == 'stopped':
+            self.stop()
+
+        if direction != self.state
             Config.update_config(config, 'Body', 'direction', direction)
             self.state = direction
 
@@ -68,10 +74,7 @@ class Body:
                 GPIO.output(36, False)
                 GPIO.output(37, False)
                 GPIO.output(38, True)
-            else: # stop
-                GPIO.output(35, False)
-                GPIO.output(36, False)
-                GPIO.output(37, False)
-                GPIO.output(38, False)
+            else:
+                self.stop()
 
             print direction
